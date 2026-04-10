602233c698914f14dd8bf5f957795e4704f8fc19
dandry <ddryuchin@gmail.com>
2026-04-10 13:43:40 +0300

    Коммит 3 в dev

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
 class Main{
     public static void main(String[] args) {
-        System.out.print("Это добавлено в ветке dev");
-        System.out.println("Изменение 2");
+        System.out.println("Это добавлено в ветке dev");
+        System.out.println("Изменение 4");
         for (int i = 1; i <= 5; i++) {
             System.out.println("Итерация №" + i);
         }
+        int score = 90;
+        if (score >= 80) {
+            System.out.println("Отличный результат!");
+        } else {
+            System.out.println("Можно лучше");
+        }
+
     }
 }
